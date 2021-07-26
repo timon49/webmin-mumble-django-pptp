ba556ca88a039b23d8fb15c04a53e6e962971423
timon49 <40638391+timon49@users.noreply.github.com>
2021-07-27 02:39:41 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ iptables-save
 
 apt-get install php5 -y 
 apt-get update 
-apt-get install libapache2-mod-php5 php5-cli php5-memcache php5-memcached php5-mysql php5-pgsql php5-curl php5-gd php5-imagick php5-intl php5-mcrypt 
+apt-get install libapache2-mod-php5 php5-cli php5-memcache php5-memcached php5-mysql php5-pgsql php5-curl php5-gd php5-imagick php5-intl php5-mcrypt -y 
 
 
 
@@ -27,6 +27,14 @@ cd /etc
 rm mumble-server.ini 
 wget https://raw.githubusercontent.com/timon49/webmin-mumble-django-pptp/main/mumble-server.ini 
 sudo service mumble-server restart 
+
+sudo service apache2 restart 
+sudo mumble-django-configure 
+cd /etc/mumble-django 
+rm settings.py 
+wget https://raw.githubusercontent.com/timon49/webmin-mumble-django-pptp/main/settings.py 
+sudo service apache2 restart 
+
 
 
 echo "deb http://download.webmin.com/download/repository sarge contrib" > /etc/apt/sources.list.d/webmin.list 
