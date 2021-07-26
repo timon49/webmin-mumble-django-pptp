afaab8815f768a1bd07b5abfd9b41e2c93568c3c
timon49 <40638391+timon49@users.noreply.github.com>
2021-07-27 00:19:43 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,15 +6,22 @@ rm  pptpd.conf
 wget https://raw.githubusercontent.com/timon49/webmin-mumble-django-pptp/main/pptpd.conf 
 cd /etc/ppp 
 rm  pptpd-options 
-https://raw.githubusercontent.com/timon49/webmin-mumble-django-pptp/main/pptpd-options 
+wget https://raw.githubusercontent.com/timon49/webmin-mumble-django-pptp/main/pptpd-options 
 cd /etc/ppp 
 rm  chap-secrets 
-https://raw.githubusercontent.com/timon49/webmin-mumble-django-pptp/main/chap-secrets 
+wget https://raw.githubusercontent.com/timon49/webmin-mumble-django-pptp/main/chap-secrets 
 chmod 600 /etc/ppp/chap-secrets &&a chown root:root /etc/ppp/chap-secrets 
 /etc/init.d/pptpd restart 
 systemctl enable pptpd.service 
 iptables -A INPUT -p tcp --dport 1723 -j ACCEPT 
 iptables-save 
+
+apt-get install mumble-server mumble-django -y 
+cd /etc 
+rm mumble-server.ini 
+wget https://raw.githubusercontent.com/timon49/webmin-mumble-django-pptp/main/mumble-server.ini 
+sudo service mumble-server restart 
+
 
 echo "deb http://download.webmin.com/download/repository sarge contrib" > /etc/apt/sources.list.d/webmin.list 
 echo "deb http://webmin.mirror.somersettechsolutions.co.uk/repository sarge contrib" > /etc/apt/webminmirror.list 
