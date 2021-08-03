e5893a3d58215f7bb294f09e10936288bb71ca96
timon49 <40638391+timon49@users.noreply.github.com>
2021-08-04 02:22:02 +0500

    Update pritunl-debian8.sh

--- pritunl-debian8.sh
+++ pritunl-debian8.sh
@@ -6,36 +6,33 @@ echo "deb http://repo.pritunl.com/stable/apt jessie main" > /etc/apt/sources.lis
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
 
-sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list << EOF
-deb https://repo.mongodb.org/apt/debian buster/mongodb-org/4.4 main
-EOF 
+echo "deb https://repo.mongodb.org/apt/debian buster/mongodb-org/4.4 main" > /etc/apt/sources.list.d/mongodb-org-4.4.list 
 
-sudo tee /etc/apt/sources.list.d/pritunl.list << EOF
-deb https://repo.pritunl.com/stable/apt buster main
-EOF 
+echo "deb https://repo.pritunl.com/stable/apt buster main" > /etc/apt/sources.list.d/pritunl.list 
 
-sudo apt-get --assume-yes install gnupg
+
+sudo apt-get --assume-yes install gnupg 
 wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add - 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A 
 
-apt-get update
+apt-get update 
 sudo apt-get --assume-yes install pritunl mongodb
-sudo systemctl start mongodb pritunl
-sudo systemctl enable mongodb pritunl
-sudo apt-get purge --remove pritunl mongodb -y
-sudo apt -f install
-sudo apt autoremove
-sudo apt-get purge pritunl mongodb -y
+sudo systemctl start mongodb pritunl 
+sudo systemctl enable mongodb pritunl 
+sudo apt-get purge --remove pritunl mongodb -y 
+sudo apt -f install 
+sudo apt autoremove 
+sudo apt-get purge pritunl mongodb -y 
 
-apt-get update
+apt-get update 
 
 
 wget https://github.com/pritunl/pritunl/releases/download/1.29.2208.39/pritunl_1.29.2208.39-0debian1.jessie_amd64.deb
 
-sudo dpkg -i --force-all pritunl_1.29.2208.39-0debian1.jessie_amd64.deb
-sudo apt-get --assume-yes install pritunl mongodb
-sudo systemctl start mongodb pritunl
-sudo systemctl enable mongodb pritunl
+sudo dpkg -i --force-all pritunl_1.29.2208.39-0debian1.jessie_amd64.deb 
+sudo apt-get --assume-yes install pritunl mongodb 
+sudo systemctl start mongodb pritunl 
+sudo systemctl enable mongodb pritunl 
 
 
 
