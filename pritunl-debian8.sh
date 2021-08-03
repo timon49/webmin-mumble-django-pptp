5bdf2af26bb1253d20fc32a543027100bf1e5032
timon49 <40638391+timon49@users.noreply.github.com>
2021-08-04 03:59:01 +0500

    Update pritunl-debian8.sh

--- pritunl-debian8.sh
+++ pritunl-debian8.sh
@@ -2,9 +2,9 @@ apt-get update
 apt-get upgrade -y 
 
 sudo apt-get --assume-yes install apt-transport-https 
-apt install mc nano git curl gnupg -y 
+apt install mc htop nano git curl gnupg -y 
 
-rm pritunl_1.29.2664.67-0debian1.jessie_amd64.deb 
+rm pritunl_1.29.2208.39-0debian1.jessie_amd64.deb 
 apt-get update -y 
 echo "deb http://repo.mongodb.org/apt/debian jessie/mongodb-org/4.0 main" > /etc/apt/sources.list.d/mongodb-org-4.0.list 
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 9DA31620334BD75D9DCB49F368818C72E52529D4 
@@ -14,8 +14,8 @@ sudo apt-get --assume-yes install gnupg
 apt install mc nano git curl gnupg -y 
 apt-get install mongodb-org -y 
 
-wget https://github.com/pritunl/pritunl/releases/download/1.29.2664.67/pritunl_1.29.2664.67-0debian1.jessie_amd64.deb 
-sudo dpkg -i --force-all pritunl_1.29.2664.67-0debian1.jessie_amd64.deb 
+wget https://github.com/pritunl/pritunl/releases/download/1.29.2208.39/pritunl_1.29.2208.39-0debian1.jessie_amd64.deb 
+sudo dpkg -i --force-all pritunl_1.29.2208.39-0debian1.jessie_amd64.deb 
 systemctl start mongod pritunl 
 systemctl enable mongod pritunl 
 sh -c 'echo "* hard nofile 64000" >> /etc/security/limits.conf' 
