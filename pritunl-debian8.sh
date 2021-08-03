8caa97d6603b256951f7056ee9f045084f6981d5
timon49 <40638391+timon49@users.noreply.github.com>
2021-08-04 03:33:56 +0500

    Update pritunl-debian8.sh

--- pritunl-debian8.sh
+++ pritunl-debian8.sh
@@ -7,35 +7,25 @@ apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 9DA31620334BD75D9DCB49
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
 
 echo "deb https://repo.mongodb.org/apt/debian buster/mongodb-org/4.4 main" > /etc/apt/sources.list.d/mongodb-org-4.4.list 
-
-echo "deb https://repo.pritunl.com/stable/apt buster main" > /etc/apt/sources.list.d/pritunl.list 
-
-
-sudo apt-get --assume-yes install gnupg 
-wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add - 
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A 
-
+rm pritunl_1.29.2664.67-0debian1.jessie_amd64.deb -y
+apt-get update -y
+apt-get upgrade -y
+echo "deb http://repo.mongodb.org/apt/debian jessie/mongodb-org/4.0 main" > /etc/apt/sources.list.d/mongodb-org-4.0.list 
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 9DA31620334BD75D9DCB49F368818C72E52529D4 
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A 
 apt-get update 
-sudo apt-get --assume-yes install pritunl mongodb
-sudo systemctl start mongodb pritunl 
-sudo systemctl enable mongodb pritunl 
-sudo apt-get purge --remove pritunl mongodb -y 
-sudo apt -f install -y 
-sudo apt-get autoremove -y 
-sudo apt-get purge pritunl mongodb -y 
+sudo apt-get --assume-yes install gnupg 
+apt install mc nano git curl gnupg -y 
+apt-get install mongodb-org -y 
 
-sudo apt -f install -y 
-
-
-apt-get update -y 
-
-
-wget https://github.com/pritunl/pritunl/releases/download/1.29.2208.39/pritunl_1.29.2208.39-0debian1.jessie_amd64.deb
-
-sudo dpkg -i --force-all pritunl_1.29.2208.39-0debian1.jessie_amd64.deb 
-sudo apt-get --assume-yes install pritunl mongodb 
-sudo systemctl start mongodb pritunl 
-sudo systemctl enable mongodb pritunl 
+wget https://github.com/pritunl/pritunl/releases/download/1.29.2664.67/pritunl_1.29.2664.67-0debian1.jessie_amd64.deb 
+sudo dpkg -i --force-all pritunl_1.29.2664.67-0debian1.jessie_amd64.deb 
+systemctl start mongod pritunl 
+systemctl enable mongod pritunl 
+sh -c 'echo "* hard nofile 64000" >> /etc/security/limits.conf' 
+sh -c 'echo "* soft nofile 64000" >> /etc/security/limits.conf' 
+sh -c 'echo "root hard nofile 64000" >> /etc/security/limits.conf' 
+sh -c 'echo "root soft nofile 64000" >> /etc/security/limits.conf' 
 
 
 
