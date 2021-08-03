6d0184beb6494b75e7bfa21e9dd650001de49dd5
timon49 <40638391+timon49@users.noreply.github.com>
2021-08-04 03:41:39 +0500

    Update pritunl-debian8.sh

--- pritunl-debian8.sh
+++ pritunl-debian8.sh
@@ -3,15 +3,9 @@ apt-get upgrade -y
 
 sudo apt-get --assume-yes install apt-transport-https 
 apt install mc nano git curl gnupg -y
-echo "deb http://repo.mongodb.org/apt/debian jessie/mongodb-org/4.0 main" > /etc/apt/sources.list.d/mongodb-org-4.0.list
-echo "deb http://repo.pritunl.com/stable/apt jessie main" > /etc/apt/sources.list.d/pritunl.list
-apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
-apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
 
-echo "deb https://repo.mongodb.org/apt/debian buster/mongodb-org/4.4 main" > /etc/apt/sources.list.d/mongodb-org-4.4.list 
 rm pritunl_1.29.2664.67-0debian1.jessie_amd64.deb -y
 apt-get update -y
-apt-get upgrade -y
 echo "deb http://repo.mongodb.org/apt/debian jessie/mongodb-org/4.0 main" > /etc/apt/sources.list.d/mongodb-org-4.0.list 
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 9DA31620334BD75D9DCB49F368818C72E52529D4 
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A 
