ea244d29a0ec98dd24080eac9715fd349f76d501
timon49 <40638391+timon49@users.noreply.github.com>
2021-08-04 03:43:33 +0500

    Update pritunl-debian8.sh

--- pritunl-debian8.sh
+++ pritunl-debian8.sh
@@ -1,15 +1,15 @@
-apt-get update
-apt-get upgrade -y
+apt-get update 
+apt-get upgrade -y 
 
 sudo apt-get --assume-yes install apt-transport-https 
-apt install mc nano git curl gnupg -y
+apt install mc nano git curl gnupg -y 
 
-rm pritunl_1.29.2664.67-0debian1.jessie_amd64.deb -y
-apt-get update -y
+rm pritunl_1.29.2664.67-0debian1.jessie_amd64.deb -y 
+apt-get update -y 
 echo "deb http://repo.mongodb.org/apt/debian jessie/mongodb-org/4.0 main" > /etc/apt/sources.list.d/mongodb-org-4.0.list 
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 9DA31620334BD75D9DCB49F368818C72E52529D4 
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A 
-apt-get update 
+apt-get update -y 
 sudo apt-get --assume-yes install gnupg 
 apt install mc nano git curl gnupg -y 
 apt-get install mongodb-org -y 
@@ -20,7 +20,7 @@ systemctl start mongod pritunl
 systemctl enable mongod pritunl 
 sh -c 'echo "* hard nofile 64000" >> /etc/security/limits.conf' 
 sh -c 'echo "* soft nofile 64000" >> /etc/security/limits.conf' 
-sh -c 'echo "root hard nofile 64000" >> /etc/security/limits.conf' 
+sh -c 'echo "root hard nofile 64000" >> /etc/security/limits.conf'  
 sh -c 'echo "root soft nofile 64000" >> /etc/security/limits.conf' 
 
 
