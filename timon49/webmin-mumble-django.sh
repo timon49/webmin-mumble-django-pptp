3e09e6ac1b778e4b43778a7e10b3031df8e30043
timon49 <40638391+timon49@users.noreply.github.com>
2021-08-04 07:32:51 +0500

    Update and rename webmin-mumble-django-pritunl.sh to webmin-mumble-django.sh

--- timon49/webmin-mumble-django.sh
+++ timon49/webmin-mumble-django.sh
@@ -16,24 +16,6 @@ apt-get install htop -y
 
 
 
-sudo apt-get --assume-yes install apt-transport-https 
-apt install mc nano git curl gnupg -y 
-
-rm pritunl-debian8.sh
-rm pritunl_1.29.2664.67-0debian1.jessie_amd64.deb
-apt-get update -y 
-echo "deb http://repo.mongodb.org/apt/debian jessie/mongodb-org/4.0 main" > /etc/apt/sources.list.d/mongodb-org-4.0.list 
-apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 9DA31620334BD75D9DCB49F368818C72E52529D4 
-apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A 
-apt-get update -y 
-sudo apt-get --assume-yes install gnupg 
-apt-get install mongodb-org -y 
-
-wget https://github.com/pritunl/pritunl/releases/download/1.29.2664.67/pritunl_1.29.2664.67-0debian1.jessie_amd64.deb
-sudo dpkg -i --force-all pritunl_1.29.2664.67-0debian1.jessie_amd64.deb
-systemctl start mongod pritunl 
-systemctl enable mongod pritunl 
-apt-get -f install -y
 
 
 apt-get install php5 -y 
