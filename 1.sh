ffb6e4e4d755550af21e6cd255d36c6257b9457d
timon49 <40638391+timon49@users.noreply.github.com>
2021-08-05 09:10:23 +0500

    Create 1.sh

---
+++ 1.sh
@@ -0,0 +1,13 @@
+echo "clear"                                                              >> .bashrc
+echo 'echo -e "\e[0m                                                   "' >> .bashrc
+echo 'echo -e "\e[94m    :::::::::  :::::::::   ::::::::  :::   :::    "' >> .bashrc
+echo 'echo -e "\e[94m    :+:    :+: :+:    :+: :+:    :+: :+:   :+:    "' >> .bashrc
+echo 'echo -e "\e[94m    +:+    +:+ +:+    +:+        +:+  +:+ +:+     "' >> .bashrc
+echo 'echo -e "\e[94m    +#+    +:+ +#++:++#:      +#++:    +#++:      "' >> .bashrc
+echo 'echo -e "\e[94m    +#+    +#+ +#+    +#+        +#+    +#+       "' >> .bashrc
+echo 'echo -e "\e[94m    #+#    #+# #+#    #+# #+#    #+#    #+#       "' >> .bashrc
+echo 'echo -e "\e[94m    #########  ###    ###  ########     ###       "' >> .bashrc
+echo 'echo -e "\e[94m           AutoScriptVPS by  _Dreyannz_           "' >> .bashrc
+echo 'echo -e "\e[0m"'                                                    >> .bashrc
+echo 'echo -e "\e[94m             [accounts/options/server]            "' >> .bashrc
+echo 'echo -e "\e[0m                                                   "' >> .bashrc
