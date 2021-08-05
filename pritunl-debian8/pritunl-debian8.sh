dea25cde12d56f2a981cf1a797a62bd146ad9bb1
timon49 <40638391+timon49@users.noreply.github.com>
2021-08-05 10:29:59 +0500

    Update pritunl-debian8.sh

--- pritunl-debian8/pritunl-debian8.sh
+++ pritunl-debian8/pritunl-debian8.sh
@@ -31,7 +31,7 @@ systemctl enable mongod pritunl
 clear
 MYIP=$(wget -qO- ipv4.icanhazip.com);
 echo -e ""
-echo -e "\e[94m<><><><><><>======================================<><><><><><>"
+echo -e "\e[94m[][][][][][][]======================================[][][][][][][]"
 echo -e "\e[0m                                                   "
 echo -e "\e[92m        AutoScript Pritunl Debian8 by timon49          "
 echo -e "\e[0m                                                   "
@@ -42,5 +42,5 @@ echo -e "\e[93m   получить ключ           :  pritunl setup-key"
 echo -e "\e[93m   получить логин и пароль :  pritunl default-password"
 echo -e "\e[93m   отключить редирект      :  pritunl set app.redirect_server false"
 echo -e "\e[0m                                                   "
-echo -e "\e[94m<><><><><><>======================================<><><><><><>\e[0m"
+echo -e "\e[94m[][][][][][][]======================================[][][][][][][]"\e[0m"
 echo -e "\e[0m                                                   " 
