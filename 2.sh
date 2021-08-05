010d8403f9a21f960efa98765650022ab98a2ebd
timon49 <40638391+timon49@users.noreply.github.com>
2021-08-05 10:11:06 +0500

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -1,6 +1,7 @@
 
-rm 2.sh
-MYIP=$(wget -qO- ipv4.icanhazip.com);
+clear
+echo -e "\e[0m                                                   "
+echo -e ""
 echo -e "\e[92m        AutoScript Pritunl Debian8 by timon49          "
 echo -e "\e[0m                                                   "
 echo -e "\e[95m            настройка pritunl  "
@@ -9,4 +10,4 @@ echo -e "\e[93m   web pritunl             :  https://$MYIP/      "
 echo -e "\e[93m   получить ключ           :  pritunl setup-key"
 echo -e "\e[93m   получить логин и пароль :  pritunl default-password"
 echo -e "\e[93m   отключить редирект      :  pritunl set app.redirect_server false"
-echo -e "\e[0m                                                   "
+echo -e "\e[0m 
