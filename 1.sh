d4f024b32e86222b35f2ef1a74e8c06a17ce4428
timon49 <40638391+timon49@users.noreply.github.com>
2021-08-05 09:29:22 +0500

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,7 +1,9 @@
+
+rm 1.sh
 echo -e "\e[92m           AutoScriptVPS by timon49          "
 echo -e "\e[0m                                                   "
 echo -e "\e[93m            web pritunl"
-echo -e "\e[93m   web pritunl             :  https://$MYIP/"
+echo -e "\e[93m   web pritunl             :  https://$MYIP      "
 echo -e "\e[93m   получить ключ           :  pritunl setup-key"
 echo -e "\e[93m   получить логин и пароль :  pritunl default-password"
 echo -e "\e[93m   отключить редирект      :  pritunl set app.redirect_server false"
