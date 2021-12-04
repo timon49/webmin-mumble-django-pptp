b0d60ec30e59c4f9c25e9d5d6306a86f1c0419c4
timon49 <40638391+timon49@users.noreply.github.com>
2021-12-04 22:01:13 +0500

    Update pritunl-debian8.sh

--- pritunl-debian8/pritunl-debian8.sh
+++ pritunl-debian8/pritunl-debian8.sh
@@ -25,14 +25,7 @@ apt-get update -y
 
 apt-get install nano -y
 
-
-echo "deb https://repo.mongodb.org/apt/debian buster/mongodb-org/4.4 main" >> /etc/apt/sources.list
-
-echo "deb https://repo.pritunl.com/stable/apt buster main" >> /etc/apt/sources.list
-
-
 sudo apt-get --assume-yes install gnupg
-
 
 wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
 
@@ -41,19 +34,21 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E528
 sudo apt-get update
 
 
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
 
-
+echo "deb http://repo.mongodb.org/apt/debian jessie/mongodb-org/3.4 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
 
 apt-get -f install -y
 
 sudo apt-get update
 
 
+
 sudo apt-get install -y mongodb-org
 
-sudo systemctl enable mongod.service mongodb-org
+sudo systemctl enable mongod.service
 
-sudo systemctl start mongod mongodb-org 
+sudo systemctl start mongod
 
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 9DA31620334BD75D9DCB49F368818C72E52529D4 
 
@@ -67,9 +62,9 @@ wget https://github.com/pritunl/pritunl/releases/download/1.29.2664.67/pritunl_1
 
 sudo dpkg -i --force-all pritunl_1.29.2664.67-0debian1.jessie_amd64.deb
 
-systemctl start mongod pritunl mongodb-org
+systemctl start mongod pritunl
 
-systemctl enable mongod pritunl mongodb-org
+systemctl enable mongod pritunl
 
 
 
